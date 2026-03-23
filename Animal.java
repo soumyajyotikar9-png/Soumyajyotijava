9e2d75c2e1cb52b21fb5f72c671315c16f6db7f7
soumyajyotikar9-png <soumyajyotikar9@gmail.com>
2026-03-23 14:12:31 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,25 @@
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+
+class Dog extends Animal {
+    
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+
+ class MethodOverridingDemo {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        Dog d = new Dog();
+
+        a.sound();  
+        d.sound();  
+    }
+}
