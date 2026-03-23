0f77c90c17f89d625f5e729ee6e1041459a72fb8
soumyajyotikar9-png <soumyajyotikar9@gmail.com>
2026-03-23 14:13:02 +0530

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class Item {
+    int code;
+    double price;
+
+    void getData(Scanner sc) {
+        System.out.print("Enter item code: ");
+        code = sc.nextInt();
+        System.out.print("Enter item price: ");
+        price = sc.nextDouble();
+    }
+
+    void putData() {
+        System.out.println(code + "\t" + price);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Item[] items = new Item[5];
+        double total = 0;
+
+        // Create objects and accept data
+        for (int i = 0; i < 5; i++) {
+            items[i] = new Item();
+            System.out.println("\nEnter details for item " + (i + 1) + ":");
+            items[i].getData(sc);
+        }
+
+        // Display data in tabular form
+        System.out.println("\nCode\tPrice");
+        System.out.println("----------------");
+
+        for (int i = 0; i < 5; i++) {
+            items[i].putData();
+            total += items[i].price;
+        }
+
+        // Display total price
+        System.out.println("----------------");
+        System.out.println("Total Price = " + total);
+
+        sc.close();
+    }
+}
