bd46df6a64eec8b49eff61fea5b81861710e707c
soumyajyotikar9-png <soumyajyotikar9@gmail.com>
2026-04-27 23:38:20 +0530

    Add files via upload

--- Hostel.java
+++ Hostel.java
@@ -1,74 +1,72 @@
 import java.util.*;
 
-// Interface
+
 interface Department {
-    void getDeptData();
-    void printDeptData();
+    String deptName = "CSE";
+    String deptHead = "Dr. Sharma";
+
+    void printDeptDetails();
 }
 
-// Hostel class
+
 class Hostel {
     String hostelName, hostelLocation;
-    int rooms;
-    Scanner sc = new Scanner(System.in);
+    int numberOfRooms;
 
     void getHostelData() {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Hostel Name: ");
         hostelName = sc.nextLine();
         System.out.print("Location: ");
         hostelLocation = sc.nextLine();
-        System.out.print("Rooms: ");
-        rooms = sc.nextInt(); sc.nextLine();
+        System.out.print("Number of Rooms: ");
+        numberOfRooms = sc.nextInt();
     }
 
     void printHostelData() {
-        System.out.println(hostelName + ", " + hostelLocation + ", Rooms: " + rooms);
+        System.out.println(hostelName + " | " + hostelLocation + " | Rooms: " + numberOfRooms);
     }
 }
 
-// Student class
+
 class Student extends Hostel implements Department {
-    String name, regdNo, subject, deptName, deptHead;
-    double marks;
-    Scanner sc = new Scanner(System.in);
+    String studentName, regdNo, electiveSubject;
+    double avgMarks;
 
     void getData() {
-        System.out.print("Name: "); name = sc.nextLine();
-        System.out.print("Regd No: "); regdNo = sc.nextLine();
-        System.out.print("Subject: "); subject = sc.nextLine();
-        System.out.print("Marks: "); marks = sc.nextDouble(); sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Student Name: ");
+        studentName = sc.nextLine();
+        System.out.print("Regd No: ");
+        regdNo = sc.nextLine();
+        System.out.print("Subject: ");
+        electiveSubject = sc.nextLine();
+        System.out.print("Avg Marks: ");
+        avgMarks = sc.nextDouble();
         getHostelData();
-        getDeptData();
     }
 
     void printData() {
-        System.out.println("\n" + name + " | " + regdNo + " | " + subject + " | " + marks);
+        System.out.println("\n--- Student Details ---");
+        System.out.println(studentName + " | " + regdNo + " | " + electiveSubject + " | " + avgMarks);
         printHostelData();
-        printDeptData();
+        printDeptDetails();
     }
 
-    public void getDeptData() {
-        System.out.print("Dept Name: "); deptName = sc.nextLine();
-        System.out.print("Dept Head: "); deptHead = sc.nextLine();
+    public void printDeptDetails() {
+        System.out.println("Dept: " + deptName + " | Head: " + deptHead);
     }
-
-    public void printDeptData() {
-        System.out.println("Dept: " + deptName + ", Head: " + deptHead);
-    }
-
-    String getRegdNo() { return regdNo; }
 }
 
-// Driver class
-class Main {
+
+ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Student> list = new ArrayList<>();
-        int ch;
 
-        do {
+        while (true) {
             System.out.println("\n1.Admit  2.Migrate  3.Display  4.Exit");
-            ch = sc.nextInt(); sc.nextLine();
+            int ch = sc.nextInt();
 
             switch (ch) {
                 case 1:
@@ -79,14 +77,18 @@ class Main {
 
                 case 2:
                     System.out.print("Enter Regd No: ");
-                    String r = sc.nextLine();
-                    list.removeIf(st -> st.getRegdNo().equals(r));
+                    String r = sc.next();
+                    list.removeIf(st -> st.regdNo.equals(r));
                     break;
 
                 case 3:
-                    for (Student st : list) st.printData();
+                    for (Student st : list)
+                        st.printData();
                     break;
+
+                case 4:
+                    return;
             }
-        } while (ch != 4);
+        }
     }
 }
